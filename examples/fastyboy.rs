2063f7836e7d409861e552359e79487c73d839f4
Tastaturtaste <31124715+Tastaturtaste@users.noreply.github.com>
2022-11-16 05:10:55 +0100

    Replace `winapi` with the official `windows` crate by Microsoft (#11)
    `winapi` hasn't been updated for a while, so it makes sense to move to the bindings that are done by the vendor themselves.

--- examples/fastyboy.rs
+++ examples/fastyboy.rs
@@ -5,11 +5,16 @@
 //! "level warmup time" to 1 second (also down from a default of 10 seconds) and disables emitters.
 //! These modifications should make the time taken to load a level in Mirror's Edge Catalyst
 //! significantly shorter.
-
-extern crate process_memory;
 #[cfg(windows)]
-extern crate winapi;
-
+mod windows {
+    pub(crate) use windows::Win32::{
+        Foundation::{CHAR, MAX_PATH},
+        System::Diagnostics::ToolHelp::{
+            CreateToolhelp32Snapshot, Process32First, Process32Next, PROCESSENTRY32,
+            TH32CS_SNAPPROCESS,
+        },
+    };
+}
 #[cfg(not(windows))]
 fn main() {
     println!("FastyBoy can only be run on systems supporting the game Mirror's Edge Catalyst, which as of writing is only Windows.")
@@ -18,17 +23,18 @@ fn main() {
 /// A helper function to get a Pid from the name of a process
 #[cfg(windows)]
 pub fn get_pid(process_name: &str) -> process_memory::Pid {
-    /// A helper function to turn a c_char array to a String
-    fn utf8_to_string(bytes: &[i8]) -> String {
+    /// A helper function to turn a CHAR array to a String
+    fn utf8_to_string(bytes: &[windows::CHAR]) -> String {
         use std::ffi::CStr;
         unsafe {
-            CStr::from_ptr(bytes.as_ptr())
+            CStr::from_ptr(bytes.as_ptr() as *const i8)
                 .to_string_lossy()
                 .into_owned()
         }
     }
-    let mut entry = winapi::um::tlhelp32::PROCESSENTRY32 {
-        dwSize: std::mem::size_of::<winapi::um::tlhelp32::PROCESSENTRY32>() as u32,
+
+    let mut entry = windows::PROCESSENTRY32 {
+        dwSize: std::mem::size_of::<windows::PROCESSENTRY32>() as u32,
         cntUsage: 0,
         th32ProcessID: 0,
         th32DefaultHeapID: 0,
@@ -37,20 +43,20 @@ pub fn get_pid(process_name: &str) -> process_memory::Pid {
         th32ParentProcessID: 0,
         pcPriClassBase: 0,
         dwFlags: 0,
-        szExeFile: [0; winapi::shared::minwindef::MAX_PATH],
+        szExeFile: [windows::CHAR(0); windows::MAX_PATH as usize],
     };
-    let snapshot: winapi::um::winnt::HANDLE;
     unsafe {
-        snapshot = winapi::um::tlhelp32::CreateToolhelp32Snapshot(
-            winapi::um::tlhelp32::TH32CS_SNAPPROCESS,
-            0,
-        );
-        if winapi::um::tlhelp32::Process32First(snapshot, &mut entry)
-            == winapi::shared::minwindef::TRUE
+        // On Error return 0 as the pid. Maybe this function should instead return itself a Result
+        // to indicate if a pid has been found?
+        let snapshot = if let Ok(snapshot) =
+            windows::CreateToolhelp32Snapshot(windows::TH32CS_SNAPPROCESS, 0)
         {
-            while winapi::um::tlhelp32::Process32Next(snapshot, &mut entry)
-                == winapi::shared::minwindef::TRUE
-            {
+            snapshot
+        } else {
+            return 0;
+        };
+        if windows::Process32First(snapshot, &mut entry) == true {
+            while windows::Process32Next(snapshot, &mut entry) == true {
                 if utf8_to_string(&entry.szExeFile) == process_name {
                     return entry.th32ProcessID;
                 }
